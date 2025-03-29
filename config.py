a2e05fcec7deef43b15e487bf1d7dcc914b1b868
Vedant Jain <vedantihs@gmail.com>
2025-03-29 09:35:36 +0530

    adjusting to .env

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HEADER = int(os.getenv('HEADER', 1024))
+PORT = int(os.getenv('PORT', 9090))
+FORMAT = os.getenv('FORMAT', 'utf-8')
+
+DISCONNECT_MESSAGE = os.getenv('DISCONNECT_MESSAGE', '!DISCONNECT')
+USER_LIST_UPDATE = os.getenv('USER_LIST_UPDATE', '!USER_LIST')
+USERNAME_TAKEN = os.getenv('USERNAME_TAKEN', 'Taken')
+USERNAME_ACCEPTED = os.getenv('USERNAME_ACCEPTED', 'Ok')
+
+WHISPER_CMD = os.getenv('WHISPER_CMD', '/w')
+DM_CMD = os.getenv('DM_CMD', '/dm')
